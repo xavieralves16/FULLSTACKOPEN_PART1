a2c3526b20f551d30eee18b290e1b3160a0eb24a
Xavier <xavieralves162@gmail.com>
2026-01-08 16:36:52 +0000

    Part 1

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -6,9 +6,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
