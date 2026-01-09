4145037b1712b4a865446fae6203ed31b08fba59
Xavier <xavieralves162@gmail.com>
2026-01-09 08:22:50 +0000

    Part 5

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,19 +1,41 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
 
-  if (props.total === 0) {
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <p>
+      {props.text} {props.value}
+    </p>
+  )
+}
+
+
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+
+  if (total === 0) {
     return <p>No feedback given</p>
   }
-  
+
+  const average = (props.good - props.bad) / total
+  const positive = (props.good / total) * 100
+
   return (
     <div>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.total}</p>
-      <p>average {props.average}</p>
-      <p>positive {props.positive} %</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + ' %'} />
     </div>
   )
 }
@@ -23,17 +45,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad
-  const average = (good - bad) / total
-  const positive = (good / total) * 100
-
   return (
     <div>
       <h1>give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
 
       <h1>statistics</h1>
 
@@ -41,12 +59,10 @@ const App = () => {
         good={good}
         neutral={neutral}
         bad={bad}
-        total={total}
-        average={average}
-        positive={positive}
       />
     </div>
   )
 }
 
 export default App
+
